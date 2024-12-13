b42e8cb8faec4d3eecb0b423828f7539acd966da
Aleksandras Bukelis <aleksbukelis@gmail.com>
2024-12-13 13:55:56 +0200

    merged conflict fix

--- PosAPI/Repositories/Interfaces/IUserRepository.cs
+++ PosAPI/Repositories/Interfaces/IUserRepository.cs
@@ -9,7 +9,6 @@ public interface IUserRepository
     Task<PaginatedResult<User>> GetAllUsersAsync(int pageNumber,int pageSize);
     Task<PaginatedResult<User>> GetAllUsersByBusinessIdAsync(int businessId,int pageNumber,int pageSize);
     Task<User?> GetUserByIdAsync(int? userId);
-    Task<List<User>> GetAllUsersByBusinessIdAsync(int businessId);
     Task<User?> GetUserByEmailAsync(string email);
     Task AddUserAsync(User user);
     Task UpdateUserAsync(User user);
