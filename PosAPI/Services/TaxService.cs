04750ea976f06daf1eedc9a94bec88f6eb66f8a0
Aleksandras-2021 <79769029+Aleksandras-2021@users.noreply.github.com>
2024-12-18 22:42:41 +0200

    Item variations that have a negative price will now have a tax of 0 (#28)

--- PosAPI/Services/TaxService.cs
+++ PosAPI/Services/TaxService.cs
@@ -7,21 +7,21 @@ using ItemCategory = PosShared.Models.Items.ItemCategory;
 
 namespace PosAPI.Services;
 
-public class TaxService: ITaxService
+public class TaxService : ITaxService
 {
     private readonly ITaxRepository _taxRepository;
-    
-    public TaxService(ITaxRepository taxRepository) =>_taxRepository = taxRepository;
-    
+
+    public TaxService(ITaxRepository taxRepository) => _taxRepository = taxRepository;
+
     public async Task<List<Tax>> GetAuthorizedTaxesAsync(User? sender)
     {
         AuthorizationHelper.Authorize("Tax", "List", sender);
-        
+
         List<Tax> taxes = new List<Tax>();
 
         if (sender.Role is UserRole.SuperAdmin)
             taxes = await _taxRepository.GetAllTaxesAsync();
-        else if(sender.Role is UserRole.Manager or UserRole.Owner or UserRole.Worker)
+        else if (sender.Role is UserRole.Manager or UserRole.Owner or UserRole.Worker)
             taxes = await _taxRepository.GetAllBusinessTaxesAsync(sender.BusinessId);
 
         return taxes;
@@ -36,23 +36,23 @@ public class TaxService: ITaxService
         if (tax == null)
             throw new KeyNotFoundException();
 
-        AuthorizationHelper.ValidateOwnershipOrRole(sender,tax.BusinessId ,sender.BusinessId, "Read");
-        
+        AuthorizationHelper.ValidateOwnershipOrRole(sender, tax.BusinessId, sender.BusinessId, "Read");
+
         return tax;
     }
 
     public async Task UpdateAuthorizedTaxAsync(Tax tax, User? sender)
     {
         AuthorizationHelper.Authorize("Tax", "Update", sender);
-        AuthorizationHelper.ValidateOwnershipOrRole(sender,tax.BusinessId ,sender.BusinessId, "Update");
-        
+        AuthorizationHelper.ValidateOwnershipOrRole(sender, tax.BusinessId, sender.BusinessId, "Update");
+
         await _taxRepository.UpdateTaxAsync(tax);
     }
 
     public async Task CreateAuthorizedTaxAsync(Tax tax, User? sender)
     {
         AuthorizationHelper.Authorize("Tax", "Create", sender);
-        
+
         await _taxRepository.AddTaxAsync(tax);
     }
 
@@ -60,17 +60,17 @@ public class TaxService: ITaxService
     {
         AuthorizationHelper.Authorize("Tax", "Delete", sender);
         Tax? tax = await _taxRepository.GetTaxByIdAsync(taxId);
-        AuthorizationHelper.ValidateOwnershipOrRole(sender,tax.BusinessId ,sender.BusinessId, "Delete");
-        
+        AuthorizationHelper.ValidateOwnershipOrRole(sender, tax.BusinessId, sender.BusinessId, "Delete");
+
         await _taxRepository.DeleteTaxAsync(taxId);
     }
 
-    public async Task<decimal> CalculateTaxByCategory(decimal itemPrice,int itemQuantity,ItemCategory itemCategory, int businessId)
+    public async Task<decimal> CalculateTaxByCategory(decimal itemPrice, int itemQuantity, ItemCategory itemCategory, int businessId)
     {
         Tax? tax = await _taxRepository.GetTaxByCategoryAsync(itemCategory, businessId);
 
         decimal totalTaxAmount;
-        if (tax != null)
+        if (tax != null && itemPrice > 0)
         {
             if (tax.IsPercentage)
                 totalTaxAmount = itemPrice * itemQuantity * tax.Amount / 100;
@@ -84,5 +84,5 @@ public class TaxService: ITaxService
 
         return totalTaxAmount;
     }
-    
+
 }
